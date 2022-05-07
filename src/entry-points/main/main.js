4d70fc36199917eec7048ba1b30692f5ddb82a53
PotapenkoVadim <potapenko.v00@mail.ru>
2022-05-07 16:34:16 +0300

    feat(extension-app): add lint & set pre-commit

--- src/entry-points/main/main.js
+++ src/entry-points/main/main.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../../applications/main/App';
 
-window.console.error = function() { void 1; };
+window.console.error = function () {
+  void 1;
+};
 
 ReactDOM.render(
   <React.StrictMode>
